d8ad59e2203e386718f52280ff8a03a083de2f90
김세화 <sehwakm@gmail.com>
2018-04-11 14:37:57 +0900

    Add root configuration and repository bean

---
+++ src/main/java/examples/teamboard/config/DBConfig.java
@@ -0,0 +1,37 @@
+package examples.teamboard.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class DBConfig {
+    @Value("${studyboard.url}")
+    private String url;
+
+    @Value("${studyboard.username}")
+    private String username;
+
+    @Value("${studyboard.password}")
+    private String password;
+
+    @Value("${studyboard.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+
+        return dataSource;
+    }
+
+}
