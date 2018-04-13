249f8651edca3f35929bd4a0a3123839472351b6
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-04-13 18:11:08 +0900

    MODIFY Test,service and DAO

--- src/test/java/examples/teamboard/dao/UserServiceTest.java
+++ src/test/java/examples/teamboard/dao/UserServiceTest.java
@@ -1,12 +1,10 @@
 package examples.teamboard.dao;
 
-import examples.teamboard.config.DBConfig;
 import examples.teamboard.config.RootApplicationContextConfig;
 import examples.teamboard.domain.User;
 import examples.teamboard.service.UserService;
 import examples.teamboard.util.SecureUtil;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +13,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
-import java.security.NoSuchAlgorithmException;
-
-
 @Rollback
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = RootApplicationContextConfig.class)
@@ -26,52 +20,65 @@ import java.security.NoSuchAlgorithmException;
 public class UserServiceTest {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserDAO userDAO;
 
     @Test
     public  void testLogin() {
         User user = new User();
-        user.setId("TestUser");
-        user.setPwd("TestUse");
-        boolean result =  userService.longIn(user);
+        user.setId("LoginTest");
+        user.setPwd(SecureUtil.sha256Encoding("LoginTest"));
+        user.setEmail("LoginTest@gmail.com");
+        user.setName("김로그");
+        user.setNickName("LoginTestNic");
+        userService.singUp(user);
 
-        Assert.assertEquals(false, result);
+        boolean result =  userService.longIn(user);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void testSingUp() {
         User user = new User();
-        user.setId("Test3");
-        user.setPwd("1234");
-        user.setEmail("testEmail3@gmail.com");
-        user.setName("김태스트3");
-        user.setNickName("Test3"); //중복된 값
-        boolean result = userService.singUp(user);
+        user.setId("singUpTest");
+        user.setPwd(SecureUtil.sha256Encoding("singUpTest"));
+        user.setEmail("singUpTest@gmail.com");
+        user.setName("김가입");
+        user.setNickName("singUpTestNic");
 
-        Assert.assertEquals(false,result);
+        boolean sing = userService.singUp(user);
+        boolean lonin = userService.longIn(user);
+
+        Assert.assertEquals(sing,lonin);
     }
 
     @Test
     public  void testFindId() {
         User user = new User();
-        user.setName("박전");
-        user.setEmail("freewifi@naver.com");
+        user.setId("FindId");
+        user.setPwd(SecureUtil.sha256Encoding("FindId"));
+        user.setEmail("FindId@gmail.com");
+        user.setName("김아이디");
+        user.setNickName("FindIdNic");
+        boolean sing = userService.singUp(user);
+
         String userId =  userService.findId(user);
 
-        Assert.assertEquals(null, userId);
+        Assert.assertEquals(user.getId(), userId);
     }
 
     @Test
     public  void testFindPwd() {
         User user = new User();
-        user.setId("freewifi");
-        user.setEmail("freewifi@naver.com");
+        user.setId("FindPwd");
+        user.setPwd(SecureUtil.sha256Encoding("FindPwd"));
+        user.setEmail("FindPwd@gmail.com");
+        user.setName("김패스");
+        user.setNickName("FindPwdNic");
+        boolean sing = userService.singUp(user);
+
         String userPwd =  userService.findPwd(user);
 
-        try {
-            Assert.assertEquals(SecureUtil.sha256Encoding("freewifi"), userPwd);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
+        Assert.assertEquals(user.getPwd(), userPwd);
     }
-
 }
