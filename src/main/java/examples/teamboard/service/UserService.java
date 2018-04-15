7d612ec85b77017205ec7df520e6484cf474e69c
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-04-15 22:06:27 +0900

    modify userService

--- src/main/java/examples/teamboard/service/UserService.java
+++ src/main/java/examples/teamboard/service/UserService.java
@@ -3,11 +3,18 @@ package examples.teamboard.service;
 import examples.teamboard.domain.User;
 import org.springframework.stereotype.Service;
 
-
 public interface UserService {
     public User longIn(User user);
+
     public boolean signUp(User user);
+
     public String findPwd(User user);
+
     public String findId(User user);
+
     public boolean checkId(User user);
+
+    public String changePwd(User user);
+
+    public String updateUser(User user);
 }
